ce5ddc9472d280a1410376efa589eb76c2d59edb
ONIX <zachem-nado@mail.ru>
2020-06-05 21:21:05 +0300

    Stage 4/5: First move! (Solution)

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,23 +5,117 @@ import java.util.Scanner;
 public class Main {
 
     static char[] field;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter cells: ");
         String s = scanner.nextLine();
         field = s.toCharArray();
+        printField(field);
+
+//        System.out.println(getTextState());
+
+        int positionOnField = enterXY();
+        doMove(positionOnField, 'X');
+        printField(field);
+    }
+
+    private static String getTextState() {
+
+        return isNotFinish() ? "Game not finished" :
+                isDraw() ? "Draw" :
+                        isWinsX() ? "X wins" :
+                                isWinsO() ? "O wins" :
+                                        isImpossible() ? "Impossible" : "Impossible";
+
+    }
+
+    private static void printField(char[] field) {
         System.out.println("---------");
         System.out.println("| " + field[0] + " " + field[1] + " " + field[2] + " |");
         System.out.println("| " + field[3] + " " + field[4] + " " + field[5] + " |");
         System.out.println("| " + field[6] + " " + field[7] + " " + field[8] + " |");
         System.out.println("---------");
+    }
 
-        System.out.println(isNotFinish() ? "Game not finished" :
-                isDraw() ? "Draw" :
-                        isWinsX() ? "X wins" :
-                                isWinsO() ? "O wins" :
-                                        isImpossible() ? "Impossible" : "Impossible");
+    private static void doMove(int positionOnField, char c) {
+        field[positionOnField] = c;
+    }
+
+    private static int enterXY() {
+        int x;
+        int y;
+        int positionOnField = 0;
+        boolean invalidCoordinates = true;
+        while (invalidCoordinates) {
+            System.out.print("Enter the coordinates: ");
+
+            try {
+                x = scanner.nextInt();
+                y = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                invalidCoordinates = true;
+                continue;
+            }
+
+            positionOnField = getPositionOnFieldFromCoordinates(x, y);
+            if (positionOnField == -1) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                invalidCoordinates = true;
+                continue;
+            }
+
+            switch (field[positionOnField]) {
+                case 'X':
+                case 'O':
+                    System.out.println("This cell is occupied! Choose another one!");
+                    invalidCoordinates = true;
+                    continue;
+            }
+            invalidCoordinates = false;
+        }
+        return positionOnField;
+    }
+
+    private static int getPositionOnFieldFromCoordinates(int x, int y) {
+        switch (x) {
+            case 1:
+                switch (y) {
+                    case 1:
+                        return 6;
+                    case 2:
+                        return 3;
+                    case 3:
+                        return 0;
+                    default:
+                        return -1;
+                }
+            case 2:
+                switch (y) {
+                    case 1:
+                        return 7;
+                    case 2:
+                        return 4;
+                    case 3:
+                        return 1;
+                    default:
+                        return -1;
+                }
+            case 3:
+                switch (y) {
+                    case 1:
+                        return 8;
+                    case 2:
+                        return 5;
+                    case 3:
+                        return 2;
+                    default:
+                        return -1;
+                }
+            default:
+                return -1;
+        }
     }
 
     /**
